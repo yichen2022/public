d59cb4e14de6239086244235f411143f878eec43
yichen2022 <64864920+yichen2022@users.noreply.github.com>
2020-12-14 21:55:08 -0500

    Add files via upload

--- OSU CSE Course and TA Database with Web Scraping/grader/app/models/course_section.rb
+++ OSU CSE Course and TA Database with Web Scraping/grader/app/models/course_section.rb
@@ -1,4 +1,3 @@
 class CourseSection < ApplicationRecord
 	has_one :section_grader_set
-	has_many :grader_application
 end
--- OSU CSE Course and TA Database with Web Scraping/grader/app/models/grader_application.rb
+++ OSU CSE Course and TA Database with Web Scraping/grader/app/models/grader_application.rb
@@ -1,10 +1,9 @@
 class GraderApplication < ApplicationRecord
-	belongs_to :course_section
 	has_many :recommendations
 	has_one :evaluation
 	has_rich_text :AvailableTimes
 	validates :Name, :presence => true, format: { with: /\A[A-Za-z]*\s[A-Za-z]*\z/ }
 	validates :InterestedCourses, format: { with: /\d{4}/, message: "must have four digits"}
 	validates :Email, format: { with: /\A[a-z]{1,}\.[1-9]{1}\d*\@[o][s][u]\.[e][d][u]\z/, message: "Email must be in the form name.#\@osu.edu"}
-	validates :GPA, format: { with: /\A\b[0-3]{1}\.\d{3}\z|\A4\.000\b\z/, message: "must be from 0-4 with 3 decimal places"}
+	validates :GPA, format: { with: /\A\b[0-3]{1}\.\d[0-3]\z|\A4\.000\b\z/, message: "must be from 0-4 with 3 decimal places"}
 end
